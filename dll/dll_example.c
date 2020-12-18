e27e80b1168b589ed163e8398516c02b2cf867ac
lesnuages <1799430+lesnuages@users.noreply.github.com>
2020-12-18 13:31:42 +0100

    Add dylib stub

--- dll/dll_example.c
+++ dll/dll_example.c
@@ -6,4 +6,14 @@ static void init(int argc, char **argv, char **envp)
     ChromeDump();
 }
 __attribute__((section(".init_array"), used)) static typeof(init) *init_p = init;
+#elif __APPLE__
+#include <stdlib.h>
+void ChromeDump();
+
+__attribute__((constructor)) static void init(int argc, char **argv, char **envp)
+{
+    unsetenv("DYLD_INSERT_LIBRARIES");
+    ChromeDump();
+}
+
 #endif
