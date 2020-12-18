ff7c8a91904d371575baa255464e8f69f0ea07f7
lesnuages <1799430+lesnuages@users.noreply.github.com>
2020-12-18 12:33:13 +0100

    Add param extraction

--- dll/dll_example.go
+++ dll/dll_example.go
@@ -9,16 +9,27 @@ import (
 )
 import (
 	"os"
+	"strings"
 )
 
 // Build with: GOOS=windows CGO_ENABLED=1 CC=/usr/bin/x86_64-w64-mingw32-gcc go build -buildmode c-shared -o chrome-dumpe.dll
 
+func getRemote(p string) string {
+	var remote string
+	s := strings.Split(p, "-remote ")
+	if len(s) > 1 {
+		remote = s[1]
+	}
+	return remote
+}
+
 //export ChromeDump
 func ChromeDump() {
+	params := os.Getenv("LD_PARAMS")
 	if runtime.GOOS != "windows" {
 		os.Unsetenv("LD_PRELOAD")
 	}
-	dump.Dump()
+	dump.Dump(getRemote(params))
 	if runtime.GOOS != "windows" {
 		os.Exit(0)
 	}
